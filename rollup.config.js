1dd59aa342de0b58f982ec57ae6355c10a8f7e12
HP Dietz <hp@jandh.org>
2020-11-14 18:45:27 +0100

    Add babel plugin to keep default export on CJS.

--- rollup.config.js
+++ rollup.config.js
@@ -105,13 +105,13 @@ export default [
           '**/*.test.js'
         ],
         plugins: [
-          '@babel/plugin-transform-runtime'
+          '@babel/plugin-transform-runtime',
+          'add-module-exports'
         ],
         presets: [
           [
             '@babel/preset-env',
             {
-              modules: false,
               targets: {
                 node: true
               }
